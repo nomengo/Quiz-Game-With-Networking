e1d352a742fa9680bb692bdc000cdff7779e9511
Levent Kırımgeri <nomengo667@gmail.com>
2020-06-08 08:35:37 -0800

    First Full Commit

---
+++ Assets/Scripts/MyData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+[System.Serializable]
+public class QuestionData
+{
+    public string Question;
+    public string A;
+    public string B;
+    public string C;
+    public string D;
+    public int CevapId;
+}
+
+public class MyData : MonoBehaviour
+{
+    public static System.Action<QuestionData> questionData;
+    public void GetDataJson()
+    {
+        StartCoroutine("GetText");
+    }
+
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost/Unity/QuestionData.php");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            var json = JSON.Parse(www.downloadHandler.text);
+            var data = new QuestionData();
+            data.Question = json["Question"].Value;
+            data.A = json["A"].Value;
+            data.B = json["B"].Value;
+            data.C = json["C"].Value;
+            data.D = json["D"].Value;
+            data.CevapId = json["Cevap_ID"].AsInt;
+
+            questionData.Invoke(data);
+        }
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject questionPanel;
+    public GameObject gameOverPanel;
+
+    private int cevap;
+    private float timer = 60f;
+    private int score = 0;
+    private bool isGameOver;
+
+
+    public Text timerText;
+    public Text scoreText;
+    public Text questionText;
+
+    public Button answerButton1;
+    public Button answerButton2;
+    public Button answerButton3;
+    public Button answerButton4;
+
+    void Start()
+    {
+        NewQuestion();
+        MyData.questionData = QuestionReceived;
+    }
+
+    private void QuestionReceived(QuestionData a)
+    {
+        questionText.text = a.Question;
+        answerButton1.GetComponentInChildren<Text>().text = a.A;
+        answerButton2.GetComponentInChildren<Text>().text = a.B;
+        answerButton3.GetComponentInChildren<Text>().text = a.C;
+        answerButton4.GetComponentInChildren<Text>().text = a.D;
+        cevap = a.CevapId;
+    }
+
+    void NewQuestion()
+    {
+        FindObjectOfType<MyData>().GetDataJson();
+    }
+    
+    public void ReturnTheButtonIndex(int le)
+    {
+        if (le == cevap)
+        {
+            NewQuestion();
+            score += 5;
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        questionPanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+    }
+    public void TextUpdate()
+    {
+        if (isGameOver)
+        {
+
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            timerText.text = "Time: " + Mathf.Round(timer).ToString();
+            scoreText.text = "Score: " + score;
+        }
+        
+    }
+
+    void Update()
+    {
+        TextUpdate();
+    }
+}
